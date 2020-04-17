b53df6b67486ac6bc08ced18a464d254ecc0e0b4
KenNguch <kennguch3@gmail.com>
2020-04-17 10:50:22 +0300

    Added Files

---
+++ src/Advance/FileIO/Main.java
@@ -0,0 +1,13 @@
+package Advance.FileIO;
+
+
+public class Main {
+    public static void main(String[] kichwa) {
+
+
+        Template template = new Template();
+        template.fileWriting();
+        template.fileReading();
+        template.getFileInfo();
+    }
+}
---
+++ src/Advance/FileIO/Template.java
@@ -0,0 +1,50 @@
+package Advance.FileIO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Template {
+    public void fileWriting() {
+
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void fileReading() {
+        try {
+            File myObj = new File("filename.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void getFileInfo() {
+        File myObj = new File("filename.txt");
+        if (myObj.exists()) {
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable " + myObj.canRead());
+            System.out.println("File size in bytes " + myObj.length());
+        } else {
+            System.out.println("The file does not exist.");
+        }
+    }
+}
